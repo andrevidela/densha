5fcc82dee5a71ad6efcf3e2f7a6cc1adcd57f846
Andre Videla <andre.videla@gmail.com>
2023-07-17 19:48:32 +0100

    Initial Commit

---
+++ densha/denshaApp.swift
@@ -0,0 +1,17 @@
+//
+//  denshaApp.swift
+//  densha
+//
+//  Created by avidela on 17/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct denshaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
